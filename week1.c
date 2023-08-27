773283dcd597e7f711f9e35f631f20bd220097b2
xsnehax <101658351+xsnehax@users.noreply.github.com>
2023-08-28 01:37:57 +0530

    Update week1.c

--- week1.c
+++ week1.c
@@ -1,6 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 #define MAX_VERTICES 10
 
@@ -9,33 +8,19 @@ int n;
 
 void DFS(int V) {
     vis[V] = 1;
-    printf("%d\n", V);
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
         if (a[V][i] == 1 && vis[i] == 0) {
             DFS(i);
         }
     }
 }
 
-bool isConnectedGraph() {
-    for (int i = 1; i <= n; i++) {
-        vis[i] = 0;
+void isConnectedDFS() {
+    for (int i = 0; i < n; i++) {
+        if (vis[i] == 0) {
+            DFS(i);
+        }
     }
-
-    // Find the first vertex that is reachable from the previous vertex
-    int startVertex = 1;
-    while (vis[startVertex] == 0)
-        startVertex++;
-
-    DFS(startVertex);
-
-    // Check if all vertices are visited
-    for (int i = 1; i <= n; i++) {
-        if (vis[i] == 0)
-            return false;
-    }
-
-    return true;
 }
 
 int main() {
@@ -45,17 +30,22 @@ int main() {
     scanf("%d", &n);
 
     printf("Enter the adjacency matrix:\n");
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= n; j++) {
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
             scanf("%d", &a[i][j]);
         }
     }
 
-    if (isConnectedGraph()) {
-        printf("The graph is connected.\n");
-    } else {
-        printf("The graph is not connected.\n");
+    isConnectedDFS();
+
+    for (i = 0; i < n; i++) {
+        if (vis[i] == 0) {
+            printf("The graph is not connected.\n");
+            return 0;
+        }
     }
+
+    printf("The graph is connected.\n");
 
     return 0;
 }
